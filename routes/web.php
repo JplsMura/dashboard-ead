0f138c72b7903de8371991c26bfa7abb80926856
JplsMura <joaopedrolima.s@hotmail.com>
2023-04-10 20:15:46 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Admin\{
+    AdminController,
+    UserController
+};
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+    Route::get('/', [AdminController::class, 'index'])->name('admin.home');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
