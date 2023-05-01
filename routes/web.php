a4e0d4d9d82f6f934662db59bd238de0ef64ff02
JplsMura <joaopedrolima.s@hotmail.com>
2023-05-01 00:00:11 -0300

    Putting routes for authentication only if it is, if you are logged in and are admin.

--- routes/web.php
+++ routes/web.php
@@ -6,51 +6,68 @@ use App\Http\Controllers\Admin\{
     DashboardController,
     LessonController,
     ModuleController,
+    ReplySupportController,
+    SupportController,
     UserController
 };
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')
+    ->middleware('auth')
+    ->group(function () {
 
-    /**
-     * Routes Lessons
-     */
-    Route::resource('/modules/{moduleId}/lessons', LessonController::class);
+        /**
+         * Reply Support
+         */
+        Route::post('/supports/{id}/reply', [ReplySupportController::class, 'store'])->name('replies.store');
 
-    /**
-     * Routes Modules
-     */
-    Route::resource('/courses/{courseId}/modules', ModuleController::class);
+        /**
+         * Supports
+         */
+        Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+        Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
-    /**
-     * Routes Courses
-     */
-    Route::resource('/courses', CourseController::class);
+        /**
+         * Routes Lessons
+         */
+        Route::resource('/modules/{moduleId}/lessons', LessonController::class);
 
-    /**
-     * Routes Admin
-     */
-    Route::put('/admins/{id}/update-image', [AdminController::class, 'uploadFile'])->name('admins.update.image');
-    Route::get('/admins/{id}/image', [AdminController::class, 'changeImage'])->name('admins.change.image');
-    Route::resource('/admins', AdminController::class);
+        /**
+         * Routes Modules
+         */
+        Route::resource('/courses/{courseId}/modules', ModuleController::class);
 
-    /**
-     * Routes Users
-     */
-    Route::put('/users/{id}/update-image', [UserController::class, 'uploadFile'])->name('users.update.image');
-    Route::get('/users/{id}/image', [UserController::class, 'changeImage'])->name('users.change.image');
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+        /**
+         * Routes Courses
+         */
+        Route::resource('/courses', CourseController::class);
 
-    Route::get('/', [DashboardController::class, 'index'])->name('admin.home');
-});
+        /**
+         * Routes Admin
+         */
+        Route::put('/admins/{id}/update-image', [AdminController::class, 'uploadFile'])->name('admins.update.image');
+        Route::get('/admins/{id}/image', [AdminController::class, 'changeImage'])->name('admins.change.image');
+        Route::resource('/admins', AdminController::class);
+
+        /**
+         * Routes Users
+         */
+        Route::put('/users/{id}/update-image', [UserController::class, 'uploadFile'])->name('users.update.image');
+        Route::get('/users/{id}/image', [UserController::class, 'changeImage'])->name('users.change.image');
+        Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+        Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+        Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+        Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+        Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+        Route::get('/', [DashboardController::class, 'index'])->name('admin.home');
+    });
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+require __DIR__ . '/auth.php';
